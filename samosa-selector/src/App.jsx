6924f31c261c1f1a7001cf96f7c7f243508863c7
liang24w <liang24w@mtholyoke.edu>
2024-03-02 14:19:39 -0500

    first commit

---
+++ samosa-selector/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Samosa Selector</h1>
+      <h2>Count: </h2>
+      <img className="Samosa" src="https://images.vexels.com/media/users/3/191119/isolated/preview/7b72fd65a91d092ba4a2b538532ff09a-indian-dish-samosa-hand-drawn.png"/>
+    </div>
+  )
+}
+
+export default App
